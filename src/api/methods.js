34700f52c70b1a64a9d4b6659879c2f488eb53fe
Jastik98 <jastik414@gmail.com>
2026-04-02 14:57:43 +0200

    sd

--- src/api/methods.js
+++ src/api/methods.js
@@ -1,20 +1,24 @@
-import request from './request'
-const googleHost = 'https://spreadsheets.google.com/feeds/cells/1V8aFVQE9vJ1mYIL-FpLBIKhlP4-aXmb1sOwfwshKvWI/1/public/values?alt=json'
-const serverHost = 'http://node2.fe.a-level.com.ua'
+import request from "./request";
+const googleHost =
+  "https://sheets.googleapis.com/v4/spreadsheets/1V8aFVQE9vJ1mYIL-FpLBIKhlP4-aXmb1sOwfwshKvWI/values/Sheet1?key=AIzaSyAkOpNzxNQDeGUTOXa4HPXguCFfXcKzQs8";
+const serverHost = "http://node2.fe.a-level.com.ua";
 
-export const getMenu = () => request({
-  method: 'GET',
-  url: googleHost,
-})
+export const getMenu = () =>
+  request({
+    method: "GET",
+    url: googleHost,
+  });
 
-export const getPosts = (data) => request({
-  method: 'POST',
-  url: `${serverHost}/get`,
-  data
-})
+export const getPosts = (data) =>
+  request({
+    method: "POST",
+    url: `${serverHost}/get`,
+    data,
+  });
 
-export const addPost = (data) => request({
-  method: 'POST',
-  url: `${serverHost}/add`,
-  data 
-})
+export const addPost = (data) =>
+  request({
+    method: "POST",
+    url: `${serverHost}/add`,
+    data,
+  });
